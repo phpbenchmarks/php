0dcf22317f8d4b774d67ee5fd962489b392f7660
Steevan BARBOYON <steevan.barboyon@gmail.com>
2017-10-24 04:10:26 +0200

    Rename rest-benchmark-type to benchmark-rest-data

--- rest.php
+++ rest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Php71Benchmark\Translation\TranslationService;
-use PhpBenchmarksRestType\{
+use PhpBenchmarksRestData\{
     Comment,
     CommentType,
     Service,
