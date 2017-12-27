3501359a6ec1b06f2f1d66de435efb36edf892bd
Steevan BARBOYON <steevan.barboyon@gmail.com>
2017-12-27 11:42:58 +0100

    Create restapi branch

--- helloworld.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-echo 'Hello World !';
-
-// require phpbenchmarks stats.php here when needed
--- init_benchmark.sh
+++ init_benchmark.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function init() {
-    composer install --no-dev --optimize-autoloader
+    composer install --no-dev --classmap-authoritative
     [ "$?" != "0" ] && exit 1
 
     return 0
