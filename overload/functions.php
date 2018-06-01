57a1f5e05465c3b78a3adce0daa83e7e36fee826
Steevan BARBOYON <steevan.barboyon@gmail.com>
2018-06-02 01:13:38 +0200

    Create Templating small overload benchmark

---
+++ overload/functions.php
@@ -0,0 +1,504 @@
+<?php
+
+declare(strict_types=1);
+
+function overloadFunction1(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction2(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction3(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction4(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction5(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction6(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction7(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction8(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction9(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction10(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction11(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction12(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction13(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction14(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction15(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction16(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction17(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction18(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction19(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction20(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction21(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction22(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction23(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction24(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction25(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction26(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction27(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction28(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction29(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction30(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction31(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction32(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction33(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction34(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction35(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction36(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction37(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction38(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction39(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction40(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction41(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction42(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction43(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction44(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction45(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction46(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction47(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction48(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction49(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction50(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction51(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction52(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction53(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction54(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction55(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction56(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction57(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction58(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction59(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction60(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction61(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction62(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction63(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction64(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction65(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction66(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction67(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction68(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction69(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction70(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction71(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction72(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction73(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction74(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction75(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction76(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction77(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction78(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction79(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction80(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction81(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction82(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction83(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction84(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction85(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction86(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction87(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction88(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction89(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction90(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction91(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction92(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction93(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction94(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction95(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction96(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction97(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction98(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction99(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
+function overloadFunction100(int $index): void
+{
+    echo '[#' . $index . '] PHP Benchmarks overload function';
+}
+
