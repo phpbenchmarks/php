9495f6cfdf7b59fcce756f5186736ccbc4a73f78
Steevan BARBOYON <steevan.barboyon@gmail.com>
2018-12-06 23:36:01 +0100

    Create REST API benchmark

---
+++ Translation/TranslationService.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Php73Benchmark\Translation;
+
+class TranslationService
+{
+    protected static $translations = false;
+
+    public static function loadTranslations(): void
+    {
+        $locales = ['fr_FR', 'en_GB', 'aa_BB'];
+        $locale = $locales[rand(0, 2)];
+
+        if (file_exists(__DIR__ . '/phpbenchmarks.' . $locale . '.php')) {
+            static::$translations = require(__DIR__ . '/phpbenchmarks.' . $locale . '.php');
+        } else {
+            static::$translations = require(__DIR__ . '/phpbenchmarks.en.php');
+        }
+    }
+
+    public static function getTranslation(string $key): string
+    {
+        return static::$translations[$key] ?? $key;
+    }
+}
