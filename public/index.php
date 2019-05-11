0f319034cca79e389c8640ddf878cc7d52030b63
Steevan <steevan.barboyon@gmail.com>
2019-05-11 21:54:00 +0200

    Change phpbenchmarks/benchmark-json-serialization-hello-world dependency name

--- public/index.php
+++ public/index.php
@@ -2,10 +2,14 @@
 
 declare(strict_types=1);
 
-if ($_GET['phpBenchmarksShowResults'] ?? null === '1') {
-    echo json_encode(['Hello World !']);
+require __DIR__ . '/../vendor/autoload.php';
+
+use PhpBenchmarks\BenchmarkJsonSerializationHelloWorld\BenchmarkService;
+
+if (BenchmarkService::isWriteToResponseBody()) {
+    echo json_encode(BenchmarkService::getDataToSerialize());
 } else {
-    json_encode(['Hello World !']);
+    json_encode(BenchmarkService::getDataToSerialize());
 }
 
 // require phpbenchmarks stats.php here when needed
