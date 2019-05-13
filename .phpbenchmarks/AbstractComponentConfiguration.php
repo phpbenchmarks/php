a0b8add484e845e641022883dbfaf1a2ce1034e8
Steevan <steevan.barboyon@gmail.com>
2019-05-13 19:03:31 +0200

    Serialization of Hello world

---
+++ .phpbenchmarks/AbstractComponentConfiguration.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace AbstractComponentConfiguration;
+
+abstract class AbstractComponentConfiguration
+{
+    /** @return int */
+    public static function getComponentType()
+    {
+        return 1;
+    }
+
+    /** @return string */
+    public static function getComponentName()
+    {
+        return 'PHP';
+    }
+
+    /** @return string */
+    public static function getComponentSlug()
+    {
+        return 'php';
+    }
+
+    /** @return bool */
+    public static function isPhp56Enabled()
+    {
+        return true;
+    }
+
+    /** @return bool */
+    public static function isPhp70Enabled()
+    {
+        return false;
+    }
+
+    /** @return bool */
+    public static function isPhp71Enabled()
+    {
+        return false;
+    }
+
+    /** @return bool */
+    public static function isPhp72Enabled()
+    {
+        return false;
+    }
+
+    /** @return bool */
+    public static function isPhp73Enabled()
+    {
+        return false;
+    }
+
+    /** @return string */
+    public static function getBenchmarkUrl()
+    {
+        return 'index.php';
+    }
+
+    /** @return string */
+    public static function getCoreDependencyName()
+    {
+        return 'php';
+    }
+
+    /** @return int */
+    public static function getCoreDependencyMajorVersion()
+    {
+        return 5;
+    }
+
+    /** @return int */
+    public static function getCoreDependencyMinorVersion()
+    {
+        return 6;
+    }
+
+    /** @return int */
+    public static function getCoreDependencyPatchVersion()
+    {
+        return 35;
+    }
+
+    /** @return int */
+    public static function getBenchmarkType()
+    {
+        return 6;
+    }
+
+    /** @return array */
+    public static function getSourceCodeUrls()
+    {
+        return [
+            'serialization' => 'https://github.com/phpbenchmarks/php/blob/php_5.6_json-serialization-hello-world/public/index.php'
+        ];
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PhpBenchmarks\BenchmarkJsonSerializationHelloWorld\BenchmarkService;
+
+if (BenchmarkService::isWriteToResponseBody()) {
+    echo json_encode(BenchmarkService::getDataToSerialize());
+} else {
+    json_encode(BenchmarkService::getDataToSerialize());
+}
+
+// require phpbenchmarks stats.php here when needed
