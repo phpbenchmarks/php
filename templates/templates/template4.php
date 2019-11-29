99efdd6cfdc0c4a3c98cbd7ce7a1863c560bd1fd
Steevan BARBOYON <steevan.barboyon@gmail.com>
2019-11-29 18:31:20 +0100

    Templating small overload benchmark for PHP 7.4.0

---
+++ templates/templates/template4.php
@@ -0,0 +1,4 @@
+                        <tr>
+                            <td>#4</td>
+                            <td><?php echo $template4 ?></td>
+                        </tr>
---
+++ templates/templates/template45.php
@@ -0,0 +1,4 @@
+                        <tr>
+                            <td>#45</td>
+                            <td><?php echo $template45 ?></td>
+                        </tr>
