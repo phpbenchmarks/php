2bd264bfa7e51be6546526c0e93e57a893fb6307
Steevan BARBOYON <steevan.barboyon@gmail.com>
2019-11-29 19:05:40 +0100

    Fix page name

--- templates/layout_start.php
+++ templates/layout_start.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
-        <title>Small overload benchmark</title>
+        <title>Big overload benchmark</title>
     </head>
     <body>
         <?php
