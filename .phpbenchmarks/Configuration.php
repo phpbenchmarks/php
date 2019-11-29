b2c4e35e99bc5c125f931bace7abab11e5892ee0
Steevan BARBOYON <steevan.barboyon@gmail.com>
2019-11-29 17:58:03 +0100

    Small JSON serialization benchmark for PHP 7.4.0

---
+++ .phpbenchmarks/Configuration.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpBenchmarks\BenchmarkConfiguration;
+
+class Configuration
+{
+    public static function getComponentType(): int
+    {
+        return 1;
+    }
+
+    public static function getComponentName(): string
+    {
+        return 'PHP';
+    }
+
+    public static function getComponentSlug(): string
+    {
+        return 'php';
+    }
+
+    public static function isCompatibleWithPhp(int $major, int $minor): bool
+    {
+        return
+            ($major === 7 && $minor === 4);
+    }
+
+    public static function getEntryPointFileName(): string
+    {
+        return 'public/index.php';
+    }
+
+    public static function getBenchmarkUrl(): string
+    {
+        return '/';
+    }
+
+    public static function getCoreDependencyName(): string
+    {
+        return 'php';
+    }
+
+    public static function getCoreDependencyMajorVersion(): int
+    {
+        return 7;
+    }
+
+    public static function getCoreDependencyMinorVersion(): int
+    {
+        return 4;
+    }
+
+    public static function getCoreDependencyPatchVersion(): int
+    {
+        return 0;
+    }
+
+    public static function getBenchmarkType(): int
+    {
+        return 7;
+    }
+
+    public static function getSourceCodeUrls(): array
+    {
+        return [
+            'jsonSerialization' => 'https://github.com/phpbenchmarks/php/blob/7.4_json-serialization-small-overload/public/index.php',
+            'integerSerialization' => 'https://github.com/phpbenchmarks/php/blob/7.4_json-serialization-small-overload/src/ObjectToSerialize/JsonSerializableToSerialize.php',
+            'floatSerialization' => 'https://github.com/phpbenchmarks/php/blob/7.4_json-serialization-small-overload/src/ObjectToSerialize/JsonSerializableToSerialize.php',
+            'stringSerialization' => 'https://github.com/phpbenchmarks/php/blob/7.4_json-serialization-small-overload/src/ObjectToSerialize/JsonSerializableToSerialize.php',
+            'booleanSerialization' => 'https://github.com/phpbenchmarks/php/blob/7.4_json-serialization-small-overload/src/ObjectToSerialize/JsonSerializableToSerialize.php',
+            'nullSerialization' => 'https://github.com/phpbenchmarks/php/blob/7.4_json-serialization-small-overload/src/ObjectToSerialize/JsonSerializableToSerialize.php',
+            'arraySerialization' => 'https://github.com/phpbenchmarks/php/blob/7.4_json-serialization-small-overload/src/ObjectToSerialize/JsonSerializableToSerialize.php',
+            'objectSerialization' => 'https://github.com/phpbenchmarks/php/blob/7.4_json-serialization-small-overload/src/ObjectToSerialize/JsonSerializableToSerialize.php'
+        ];
+    }
+}
