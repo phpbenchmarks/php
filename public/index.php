85151d30a78fc82b4379d8db2a4d6b33d8bfcdf3
Steevan BARBOYON <steevan.barboyon@gmail.com>
2020-02-12 16:27:51 +0100

    Corrections for Benchmark kit 4

--- public/index.php
+++ public/index.php
@@ -2,22 +2,57 @@
 
 declare(strict_types=1);
 
-use App\{
-    ArrayEncoder\UserArrayEncoder,
-    Translation\TranslationService
+use App\Translation\TranslationService;
+use PhpBenchmarksRestData\{
+    Comment,
+    CommentType,
+    Service,
+    User
 };
-use PhpBenchmarksRestData\Service;
 
-require('../vendor/autoload.php');
+require(__DIR__ . '/../vendor/autoload.php');
+
+function encodeUser(User $user): array
+{
+    return [
+        'id' => $user->getId(),
+        'login' => $user->getLogin(),
+        'createdAt' => $user->getCreatedAt()->format('Y-m-d H:i:s'),
+        'translated' => TranslationService::getTranslation('translated.1000'),
+        'comments' => encodeComments($user->getComments())
+    ];
+}
+
+/** @param Comment[] $comments */
+function encodeComments(array $comments): array
+{
+    $return = [];
+    foreach ($comments as $comment) {
+        $return[] = [
+            'id' => $comment->getId(),
+            'message' => $comment->getMessage(),
+            'translated' => TranslationService::getTranslation('translated.2000'),
+            'type' => encodeType($comment->getType())
+        ];
+    }
+
+    return $return;
+}
+
+function encodeType(CommentType $type): array
+{
+    return [
+        'id' => $type->getId(),
+        'name' => $type->getName(),
+        'translated' => TranslationService::getTranslation('translated.3000')
+    ];
+}
 
 TranslationService::loadTranslations();
 
-$userEncoder = new UserArrayEncoder();
 $data = [];
 foreach (Service::getUsers() as $user) {
-    $data[] = $userEncoder->encode($user);
+    $data[] = encodeUser($user);
 }
 
 echo json_encode($data);
-
-// require phpbenchmarks stats.php here when needed
