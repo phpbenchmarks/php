1dc4ab9be20280b83955a2e2c0b0c8b708bcdc57
Steevan BARBOYON <steevan.barboyon@gmail.com>
2020-02-14 14:44:40 +0100

    Add preload

--- .phpbenchmarks/php/7.4/preload.php
+++ .phpbenchmarks/php/7.4/preload.php
@@ -1,1 +1,13 @@
 <?php
+
+declare(strict_types=1);
+
+opcache_compile_file(__DIR__ . '/../../../public/index.php');
+
+// If somebody find why when I add any file in src/ into preload,
+// PHP-FPM restart whithout error but i have a white page or a 502 with only this log:
+// WARNING: [pool www] child 2854 exited on signal 6 (SIGABRT - core dumped) after 0.154003 seconds from start
+// I think it's because of the size of the preloaded files
+//opcache_compile_file(__DIR__ . '/../../../src/blocks.php');
+//opcache_compile_file(__DIR__ . '/../../../src/functions.php');
+//opcache_compile_file(__DIR__ . '/../../../src/macros.php');
