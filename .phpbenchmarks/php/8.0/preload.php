c743bac102d3c0236d45f29cf4fc016118560997
Steevan BARBOYON <steevan.barboyon@gmail.com>
2020-12-01 18:10:03 +0100

    REST API benchmark for PHP 8.0.0

---
+++ .phpbenchmarks/php/8.0/preload.php
@@ -0,0 +1,23 @@
+<?php
+
+$sourceCodePath = __DIR__ . '/../../../';
+$files = [
+    "$sourceCodePath/public/index.php",
+    "$sourceCodePath/src/Translation/TranslationService.php",
+    "$sourceCodePath/translations/phpbenchmarks.en.php",
+    "$sourceCodePath/translations/phpbenchmarks.en_GB.php",
+    "$sourceCodePath/translations/phpbenchmarks.fr_FR.php",
+    "$sourceCodePath/vendor/autoload.php",
+    "$sourceCodePath/vendor/phpbenchmarks/benchmark-rest-data/Comment.php",
+    "$sourceCodePath/vendor/phpbenchmarks/benchmark-rest-data/CommentType.php",
+    "$sourceCodePath/vendor/phpbenchmarks/benchmark-rest-data/Service.php",
+    "$sourceCodePath/vendor/phpbenchmarks/benchmark-rest-data/User.php",
+];
+
+foreach ($files as $file) {
+    if (is_readable($file) === false) {
+        throw new \Exception('File "' . $file . '" is not readable.');
+    }
+
+    opcache_compile_file($file);
+}
---
+++ .phpbenchmarks23/php/7.4/preload.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+$rootDir = __DIR__ . '/../../..';
+
+require($rootDir . '/translations/phpbenchmarks.en.php');
+require($rootDir . '/translations/phpbenchmarks.en_GB.php');
+require($rootDir . '/translations/phpbenchmarks.fr_FR.php');
+require($rootDir . '/public/index.php');
